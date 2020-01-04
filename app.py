b2541641c2e9f956277ab6e0cc4ed2afe85e688f
Ubuntu <ubuntu@ip-172-31-82-227.ec2.internal>
2020-01-04 09:41:47 +0000

    install flask and pymongo packages

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+import pymongo
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__) 
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
