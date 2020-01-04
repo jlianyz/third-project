12d64120d3e3beea526b257703498c95216454dc
Ubuntu <ubuntu@ip-172-31-82-227.ec2.internal>
2020-01-04 09:54:37 +0000

    linked mongodb to list products page

--- app.py
+++ app.py
@@ -20,6 +20,38 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html')
 
+
+@app.route('/list_products')
+def list_products():
+    type = request.args.get('type')
+    purpose = request.args.get('purpose')
+    origin = request.args.get('origin')
+    criteria= {} 
+    
+    if type and type != 'Type':
+        criteria['type'] = type
+    else:
+        type = 'Type'
+        
+    if purpose and purpose != 'Purpose':
+        criteria['purpose'] = purpose
+    else:
+        purpose ='Purpose'
+        
+    if origin and origin != 'Origin':
+        criteria['origin'] = origin
+    else:
+        origin = 'Origin'
+        
+        
+    products = conn[DATABASE_NAME][COLLECTION_NAME].find(criteria)
+    print(products)
+    types = conn[DATABASE_NAME][COLLECTION_NAME2].find()
+    purposes = conn[DATABASE_NAME][COLLECTION_NAME3].find()
+    origins = conn[DATABASE_NAME][COLLECTION_NAME4].find()
+    return render_template("list_products.html", 
+                           products=products,type=type, purpose=purpose, origin=origin, types=types, purposes=purposes, origins=origins)
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
