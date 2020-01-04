c1faf8c2086aa269a5451a29b54c8e3174d19691
Ubuntu <ubuntu@ip-172-31-82-227.ec2.internal>
2020-01-04 09:47:34 +0000

    linked app py file to mongodb

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__) 
 
+app.config["MONGO_DBNAME"] = 'myDatabase'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+conn = pymongo.MongoClient(app.config["MONGO_URI"])
+DATABASE_NAME = "myDatabase"
+COLLECTION_NAME = "products"
+COLLECTION_NAME2 = "types"
+COLLECTION_NAME3 = "purpose"
+COLLECTION_NAME4 = "origin"
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
